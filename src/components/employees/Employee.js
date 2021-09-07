5cdcd1bf4f07d0a60b1cacc4d6cfb8908e00efe9
Erin <etruman2@gmail.com>
2021-09-07 14:16:55 -0500

    delete button functionality creates on Employees.js

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useHistory } from "react-router-dom"
 import EmployeeRepository from "../../repositories/EmployeeRepository";
 import useResourceResolver from "../../hooks/resource/useResourceResolver";
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
@@ -7,13 +7,16 @@ import person from "./person.png"
 import "./Employee.css"
 
 
-export default ({ employee }) => {
+export default ({ employee, updateEmployees }) => { // object deconstruction, employee is a child of employee list, so I can pass props down from employee list to employee
     const [animalCount, setCount] = useState(0)
     const [location, markLocation] = useState({ name: "" })
     const [classes, defineClasses] = useState("card employee")
     const { employeeId } = useParams()
     const { getCurrentUser } = useSimpleAuth()
     const { resolveResource, resource } = useResourceResolver()
+
+
+
 
     useEffect(() => {
         if (employeeId) {
@@ -34,22 +37,9 @@ export default ({ employee }) => {
         EmployeeRepository.delete(id)
         .then(() => {
            EmployeeRepository.getAll()
+           .then(updateEmployees)
         })
     }
-
-
-
-    // const deleteTicket = (id) => {
-    //     fetch(`http://localhost:8088/serviceTickets/${id}`, {
-    //         method: "DELETE"
-    //     })
-    //     .then(() => {
-    //         window.location.reload(false);
-    //     })
-    // }
-
-
-   
 
 
     return (
